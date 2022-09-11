5b5e0662de90ed665c63886b928ec80081b17514
Norman Basham <2135673+nbasham@users.noreply.github.com>
2022-09-11 16:53:47 -0700

    Access fix.

--- Sources/SudokuPuzzles/SudokuPuzzles.swift
+++ Sources/SudokuPuzzles/SudokuPuzzles.swift
@@ -1,18 +1,9 @@
 import SwiftUI
 
 public struct SudokuPuzzles {
-/*    public static let all = Puzzles(files: [("puzzles_easy.json", .easy),
-                                      ("puzzles_medium.json", .medium),
-                                      ("puzzles_hard.json", .hard),
-                                      ("puzzles_evil.json", .evil)])
-    public static let test = Puzzles(files: [("test_puzzles_easy.json", .easy),
-                                             ("test_puzzles_medium.json", .medium),
-                                             ("test_puzzles_hard.json", .hard),
-                                             ("test_puzzles_evil.json", .evil)])
-*/
-    static let all = Puzzles(files: PuzzleDifficultyLevel.allCases.map { ("puzzles/puzzles_\($0.description.lowercased()).json", $0) })
+    public static let all = Puzzles(files: PuzzleDifficultyLevel.allCases.map { ("puzzles/puzzles_\($0.description.lowercased()).json", $0) })
 
-    static let test = Puzzles(files: PuzzleDifficultyLevel.allCases.map { ("puzzles/test_puzzles_\($0.description.lowercased()).json", $0) })
+    public static let test = Puzzles(files: PuzzleDifficultyLevel.allCases.map { ("puzzles/test_puzzles_\($0.description.lowercased()).json", $0) })
 }
 
 public class Puzzles {
